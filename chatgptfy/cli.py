d1a9f1c88e2d24806a030b78ff9c7f78311fef13
anan <anan@example.com>
2023-03-25 19:06:47 +0900

    :construction: add options

--- chatgptfy/cli.py
+++ chatgptfy/cli.py
@@ -1,14 +1,15 @@
 import openai
 import os
-from chatgptfy.models import Base, Context, Message
+import click
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+from models import Base, Context, Message
 
 OPENAI_API_KEY = os.environ.get("OPENAI_API_KEY")
 HOME = os.environ.get("HOME")
 CONTEXT_DB = "{}/.config/chatgptfy/context.db".format(HOME)
 openai.api_key = OPENAI_API_KEY
-
+GPT_MODEL = "gpt-3.5-turbo"
 
 class Chatgptfy:
     def __init__(self):
@@ -33,8 +34,8 @@ class Chatgptfy:
     def get_messages(self, context):
         return context.messages
 
-    def add_message(self, session, context, message, role='user'):
-        message = Message(role=role, content=message)
+    def add_message(self, session, context, content, role='user'):
+        message = Message(role=role, content=content)
         context.messages = [message]
         session.add(context)
 
@@ -46,162 +47,103 @@ class Chatgptfy:
     def drop_database(self):
         Base.metadata.drop_all(self.engine)
 
-    # def send_question_to_chatgpt_api(
-    #         self,
-    #         messages,
-    #         max_tokens=150) -> str:
-    #     response = openai.ChatCompletion.create(
-    #         model="gpt-3.5-turbo",
-    #         messages=messages,
-    #         max_tokens=max_tokens,
-    #         n=1,
-    #         temperature=0.5,
-    #     )
-    #     return response.choices[0].message['content']
+    def send_question_to_chatgpt_api(
+            self,
+            messages,
+            max_tokens=150,
+            temprature=0.5
+            ) -> Message:
+        dict_messages = [{'role': message.role,
+                          'content': message.content}
+                         for message in messages]
+        response = openai.ChatCompletion.create(
+            model=GPT_MODEL,
+            messages=dict_messages,
+            max_tokens=max_tokens,
+            n=1,
+            temperature=temprature,
+        )
+        if (response):
+            total_tokens = response['usage']['total_tokens']
+            content = response['choices'][0]['message']['content']
+            finish_reason = response['choices'][0]['finish_reason']
+            message = Message(role='assistant',
+                              content=content,
+                              total_tokens=total_tokens,
+                              finish_reason=finish_reason)
+            return message
+        else:
+            raise Exception("No response from OpenAI API")
 
 
+@click.command()
+@click.option('--system', is_flag=True)
+@click.option('--context', default='default', help='Context to use')
+@click.option('--message', default='Hello', help='Message to send')
+@click.option('--max-tokens', default=150, help='Max tokens to use')
+@click.option('--temperature', default=0.5, help='Temperature to use')
+@click.option('--list-contexts', help='List contexts')
+@click.option('--list-messages', default='default', help='List messages for context')
+@click.option('--init-db', is_flag=True)
+@click.option('--drop-db', is_flag=True)
+def main(system,
+         context,
+         message,
+         max_tokens,
+         temperature,
+         list_contexts,
+         list_messages,
+         init_db,
+         drop_db
+         ):
+    chatgptfy = Chatgptfy()
+    user = 'user'
+    if system:
+        user = 'system'
+    if init_db:
+        chatgptfy.init_database()
+        return
+    if drop_db:
+        chatgptfy.drop_database()
+        return
+    if list_contexts:
+        session = chatgptfy.get_session()
+        contexts = chatgptfy.get_contexts(session)
+        for context in contexts:
+            print(context.title)
+        return
+    if list_messages:
+        session = chatgptfy.get_session()
+        context = chatgptfy.get_context(session, list_messages)
+        if context is None:
+            print("No context found")
+            return
+        messages = chatgptfy.get_messages(context)
+        for message in messages:
+            print(message.role, message.content)
+        return
+    else:
+        session = chatgptfy.get_session()
+        context = chatgptfy.get_context(session, context)
+        if not context:
+            context = chatgptfy.add_context(session, "default")
+        chatgptfy.add_message(session, context, message)
+        messages = chatgptfy.get_messages(context)
+        message = Message(role=user, content=message)
+        messages.append(message)
+        response = chatgptfy.send_question_to_chatgpt_api(
+            messages,
+            max_tokens=max_tokens,
+            temprature=temperature
+            )
+        chatgptfy.add_message(session, message.context, message.role)
+        chatgptfy.add_message(session,
+                              context,
+                              response.content,
+                              role='assistant')
+        session.commit()
+        print(response.content)
 
-
-
-
-
-# act_as = {
-#        "Translator": {
-#            "role": "system",
-#            "content": """
-#            I want you to act as an English translator,
-#             spelling corrector and improver.
-#             I will speak to you in any language
-#             and you will detect the language,
-#             translate it and answer in the corrected
-#             and improved version of my text, in English.
-#             I want you to replace my simplified A0-level words
-#             and sentences with more beautiful and elegant,
-#             upper level English words and sentences.
-#             Keep the meaning same,
-#             but make them more literary.
-#             I want you to only reply the correction,
-#             the improvements and nothing else,
-#             do not write explanations."
-#            """,
-#            },
-#        "LaTeX": {
-#            "role": "system",
-#            "content": """
-#            I want you to act as a LaTeX editor.
-#            I will speak to you in any language
-#            and you will detect the language,
-#            Answer in the corrected
-#            and improved version of my text in LaTeX form.
-#            I want you to replace my simplified A0-level words
-#            and sentences with more beautiful and elegant,
-#            upper level words and sentences.
-#            Keep the meaning same,
-#            but make them more literary.
-#            I want you to only reply the correction,
-#            the improvements and nothing else,
-#            do not write explanations."
-#            """,
-#            },
-#        "Extractor": {
-#            "role": "system",
-#            "content": """
-#            あなたは日本語の文章からキーワードを抽出するアシスタントAIです。userが入力した文章から、検索観点でインデックスするべき重要なキーワードを10個を','区切りで改行なしで出力しなさい。
-#            """,
-#            },
-#        }
-#
-# def load_history() -> List[Dict[str, str]]:
-#     try:
-#         with open(HISTORY_FILENAME, "r") as f:
-#             return json.load(f)
-#     except FileNotFoundError:
-#         return []
-#
-#
-# def save_history(history: List[Dict[str, str]]) -> None:
-#     with open(HISTORY_FILENAME, "w") as f:
-#         json.dump(history, f)
-#
-#
-# def send_question_to_chatgpt_api(messages: List[Dict[str, str]], max_tokens=150) -> str:
-#     response = openai.ChatCompletion.create(
-#         model="gpt-3.5-turbo",
-#         messages=messages,
-#         max_tokens=max_tokens,
-#         n=1,
-#         temperature=0.5,
-#     )
-#
-#     return response.choices[0].message['content']
-#
-#
-# def clear_history() -> None:
-#     with open(HISTORY_FILENAME, "w") as f:
-#         json.dump([], f)
-#
-#
-# def main() -> None:
-#     try:
-#         parser = argparse.ArgumentParser()
-#         parser.add_argument("--clear",
-#                             help="Clears the question history",
-#                             action="store_true")
-#         parser.add_argument("-r", "--role", help="Act as a role", type=str)
-#         parser.add_argument("-t", "--maxtokens", help="Max tokens to consume.", type=int)
-#         parser.add_argument("-m", "--message", help="Message to ChatGPT", type=str)
-#         parser.add_argument("-q", "--quiet", help="Quiet mode", action="store_true")
-#         parser.add_argument("-k", "--keep-message", help="Keep message in output", action="store_true")
-#         args = parser.parse_args()
-#
-#         if args.clear:
-#             clear_history()
-#             print("History cleared.")
-#             sys.exit()
-#
-#         act_as.setdefault(args.role, {
-#                 "role": "system",
-#                 "content": f"You are {args.role}.",
-#             })
-#
-#         history = load_history()
-#         question = ""
-#
-#         if sys.stdin.isatty() and not args.message and not args.quiet:
-#             print("Enter your question:")
-#             question = input()
-#         elif args.message:
-#             question = args.message
-#         else:
-#             question = input()
-#
-#         messages = [{
-#             "role": "system",
-#             "content": "I want yout to act as an ChatGPT assistant.",
-#         }]
-#         for entry in history:
-#             messages.append({"role": "user", "content": f"Q: {entry['question']}"})
-#             messages.append({"role": "assistant",
-#                              "content": f"A: {entry['answer']}"})
-#         messages = [act_as[args.role]]
-#         messages.append({"role": "user", "content": question})
-#
-#         answer = send_question_to_chatgpt_api(messages, max_tokens=args.maxtokens)
-#
-#         history.append({"question": question, "answer": answer})
-#         save_history(history)
-#
-#         if sys.stdin.isatty() and not args.quiet:
-#             if args.keep_message:
-#                 print(f"Question:\n{question}\n")
-#             print(f"Answer:\n{answer}")
-#         else:
-#             if args.keep_message:
-#                 answer = f"{question}\n {answer}"
-#             print(f"{answer}")
-#     except KeyboardInterrupt:
-#         print("bye!")
-#
 
 if __name__ == "__main__":
     main()
---
+++ chatgptfy/models.py
@@ -0,0 +1,42 @@
+from sqlalchemy import Column
+from sqlalchemy import Integer
+from sqlalchemy import String
+from sqlalchemy import ForeignKey
+from sqlalchemy.orm import relationship
+from sqlalchemy.ext.declarative import declarative_base
+
+
+Base = declarative_base()
+
+
+class Context(Base):
+    __tablename__ = 'context'
+    id = Column(Integer, primary_key=True)
+    messages = relationship('Message', back_populates='context')
+    title = Column(String(50))
+
+    def __repr__(self):
+        return f'Context(id={self.id})'
+
+
+class Message(Base):
+    __tablename__ = 'message'
+    id = Column(Integer, primary_key=True)
+    role = Column(String(50))
+    content = Column(String(50))
+    timestamp = Column(String(50))
+    total_tokens = Column(Integer)
+    finish_reason = Column(String(50))
+    context_id = Column(Integer, ForeignKey('context.id'))
+    context = relationship('Context', back_populates='messages')
+
+    def __repr__(self):
+        return f"""
+                 Message(
+                    id={self.id},
+                    role={self.role},
+                    content={self.message},
+                    timestamp={self.timestamp},
+                    context_id={self.context_id}
+                 )
+               """
